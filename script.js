7fd0f21cd55ce5280f9cf6523796dbe36b2ed9eb
takumi <0520mikuta0@gmail.com>
2026-02-04 18:01:28 +0900

    チェック機能追加、CSS追加

--- script.js
+++ script.js
@@ -2,15 +2,48 @@ const textInput = document.getElementById("text-input");
 const addButton = document.getElementById("add-btn");
 const todoArea = document.getElementById("todo-area");
 
+function createTodoItem(text) {
+  const todoItem = document.createElement("div");
+  todoItem.classList.add("todo-item");
+
+  const todoCheckbox = document.createElement("input");
+  todoCheckbox.type = "checkbox";
+  todoCheckbox.classList.add("complete-checkbox");
+  todoItem.appendChild(todoCheckbox);
+
+  const todoText = document.createElement("span");
+  todoText.textContent = text;
+  todoText.classList.add("todo-text");
+  todoItem.appendChild(todoText);
+
+  const deleteButton = document.createElement("button");
+  deleteButton.textContent = "削除";
+  deleteButton.classList.add("delete-btn");
+  todoItem.appendChild(deleteButton);
+
+  todoArea.appendChild(todoItem);
+}
+
 addButton.addEventListener("click", () => {
   if (textInput.value === "") return;
 
-  console.log(textInput.value);
-
-  const todoItem = document.createElement("div");
-  todoItem.textContent = textInput.value;
-  todoArea.appendChild(todoItem);
+  createTodoItem(textInput.value);
 
   textInput.value = "";
   textInput.focus();
+});
+
+todoArea.addEventListener("click", (event) => {
+  // 削除ボタンが押された場合
+  if (event.target.classList.contains("delete-btn")) {
+    const clickedTodoItem = event.target.closest(".todo-item");
+    clickedTodoItem.remove();
+  }
+
+  // チェックボックスが押された場合
+  if (event.target.classList.contains("complete-checkbox")) {
+    const clickedTodoItem = event.target.closest(".todo-item");
+    const clickedTodoText = clickedTodoItem.querySelector(".todo-text");
+    clickedTodoText.classList.toggle("completed");
+  }
 });
