d70bbed836fd951449ec693342e66f75866ae23e
takumi <0520mikuta0@gmail.com>
2026-02-09 22:14:09 +0900

    localStrage復元処理を含むToDo基本機能を実装

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const textInput = document.getElementById("text-input");
 const addButton = document.getElementById("add-btn");
 const todoArea = document.getElementById("todo-area");
-const todos = [];
+let todos = [];
 
 // 1行DOMの作成
 function createTodoItem(text, completed, index) {
@@ -41,11 +41,33 @@ function renderTodos() {
     // todosを１行ずつ読み取る
     const text = todo.text;
     const completed = todo.completed;
-    console.log(`text:${text}, completed:${completed}, index:${index}`);
+    // console.log(`text:${text}, completed:${completed}, index:${index}`);
 
     // 1行DOMの作成→todoAreaに表示
     todoArea.append(createTodoItem(text, completed, index));
   });
+}
+
+// localStorageに保存
+function saveTodos() {
+  localStorage.setItem("todos", JSON.stringify(todos));
+}
+
+// localStrageに保存してあるtodosを復元
+const savedTodos = localStorage.getItem("todos");
+
+// localStrageにundefinedが入って、エラーを起こすのを防ぐ処理
+// "undefined"という文字列ではなく、かつ値が存在する場合のみ実行
+if (savedTodos && savedTodos !== "undefined") {
+  try {
+    todos = JSON.parse(savedTodos);
+    renderTodos();
+  } catch (e) {
+    // もしJSONが壊れていたら、空の配列でリセットする処理
+    console.error("データの読み込みに失敗しました", e);
+    todos = [];
+    localStorage.removeItem("todos");
+  }
 }
 
 addButton.addEventListener("click", () => {
@@ -57,6 +79,7 @@ addButton.addEventListener("click", () => {
     completed: false,
   });
 
+  saveTodos();
   renderTodos();
 
   textInput.value = "";
@@ -67,19 +90,22 @@ todoArea.addEventListener("click", (event) => {
   // 削除ボタンが押された場合
   if (event.target.classList.contains("delete-btn")) {
     const clickedTodoItem = event.target.closest(".todo-item");
-    clickedTodoItem.remove();
+    const index = Number(clickedTodoItem.dataset.index);
+
+    todos.splice(index, 1);
+
+    saveTodos();
+    renderTodos();
   }
 
   // チェックボックスが押された場合
   if (event.target.type == "checkbox") {
-    const isChecked = event.target.checked;
     const clickedTodoItem = event.target.closest(".todo-item");
-    const clickedTodoText = clickedTodoItem.querySelector(".todo-text");
+    const index = Number(clickedTodoItem.dataset.index);
 
-    if (isChecked) {
-      clickedTodoText.classList.add("completed");
-    } else {
-      clickedTodoText.classList.remove("completed");
-    }
+    todos[index].completed = event.target.checked;
+
+    saveTodos();
+    renderTodos();
   }
 });
